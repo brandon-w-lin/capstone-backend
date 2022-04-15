1b4f32bb23f5540e8331ba1284c41d4a4e48bdd0
Brandon Lin <bwlin93@gmail.com>
2022-04-15 14:49:37 -0500

    renames from 'capstone-frontend' to 'capstone-backend'

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,54 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Book.create(
+  isbn: 9780590353427,
+  title: "Harry Potter and the Sorcerer's Stone",
+  summary: "Harry Potter spent ten long years living with Mr. and Mrs. Dursley, an aunt and uncle whose outrageous favoritism of their perfectly awful son Dudley leads to some of the most inspired dark comedy since Charlie and the Chocolate Factory. But fortunately for Harry, he's about to be granted a scholarship to a unique boarding school called THE HOGWORTS SCHOOL OF WITCHCRAFT AND WIZARDRY, where he will become a school hero at the game of Quidditch (a kind of aerial soccer played high above the ground on broomsticks), he will make some wonderful friends, and, unfortunately, a few terrible enemies. For although he seems to be getting your run-of-the-mill boarding school experience (well, ok, even that's pretty darn out of the ordinary), Harry Potter has a destiny that he was born to fulfill. A destiny that others would kill to keep him from.",
+  author: "J.K. Rowling",
+  publisher: "Scholastic, Inc.",
+  cover_image_url: "https://images-na.ssl-images-amazon.com/images/I/51DF6ZR8G7L._SX329_BO1,204,203,200_.jpg",
+  genre: "Fantasy",
+  year: 1998,
+)
+Book.create(
+  isbn: 9780618002221,
+  title: "Harry Potter and the Sorcerer's Stone",
+  summary: "In ancient times the Rings of Power were crafted by the Elven-smiths, and Sauron, the Dark Lord, forged the One Ring, filling it with his own power so that he could rule all others. But the One Ring was taken from him, and though he sought it throughout Middle-earth, it remained lost to him. After many ages it fell into the hands of Bilbo Baggins, as told in THE HOBBIT. In a sleepy village in the Shire, young Frodo Baggins finds himself faced with an immense task, as his elderly cousin Bilbo entrusts the Ring to his care. Frodo must leave his home and make a perilous journey across Middle-earth to the Cracks of Doom, there to destroy the Ring and foil the Dark Lord in his evil purpose.",
+  author: "J.R.R. Tolkien",
+  publisher: "Houghton Mifflin Harcourt",
+  cover_image_url: "https://d3525k1ryd2155.cloudfront.net/f/221/002/9780618002221.BX.0.m.jpg",
+  genre: "Fantasy",
+  year: 1999,
+)
+
+Song.create(
+  url: "https://www.youtube.com/watch?v=wtHra9tFISY&ab_channel=JohnWilliams-Topic",
+  title: "Hedwig's Theme",
+  artist: "John Williams",
+  album: "Harry Potter and the Philosopher's Stone",
+  genre: "Movie Score",
+  year: 2001,
+)
+
+Song.create(
+  url: "https://www.youtube.com/watch?v=vbKy6FuACKg",
+  title: "The Bridge of Khazad Dum",
+  artist: "Howard Shore",
+  album: "The Lord of the Rings: The Fellowship of the Ring",
+  genre: "Movie Score",
+  year: 2001,
+)
+Song.create(
+  url: "https://www.youtube.com/watch?v=bGkBHKaGM5w",
+  title: "The Black Rider",
+  artist: "Howard Shore",
+  album: "The Lord of the Rings: The Fellowship of the Ring",
+  genre: "Movie Score",
+  year: 2001,
+)
+
+# BookSong.create(book_id: 1, song_id: 1)
+# Booksong.create(book_id: 2, song_id: 2)
+# Booksong.create(book_id: 1, song_id: 3)
