b483aeffbcf0b31c37c81c5582db84228b336a7a
Brandon Lin <bwlin93@gmail.com>
2022-04-15 14:06:13 -0500

    recreates book model without booksong_id

--- db/migrate/20220415190555_create_books.rb
+++ db/migrate/20220415190555_create_books.rb
@@ -1,7 +1,6 @@
 class CreateBooks < ActiveRecord::Migration[7.0]
   def change
     create_table :books do |t|
-      t.integer :booksong_id
       t.string :isbn
       t.string :title
       t.text :summary
