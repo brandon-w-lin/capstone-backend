815e6f0e5c726359cfa21fc86b0b61987ab0329a
Brandon Lin <bwlin93@gmail.com>
2022-04-21 13:27:06 -0500

    relabels from bookssongs to booksongs

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,4 +1,4 @@
 class Song < ApplicationRecord
-  has_many :bookssongs, foreign_key: "song_id", class_name: "BookSong"
-  has_many :books, through: :bookssongs
+  has_many :booksongs, foreign_key: "song_id", class_name: "BookSong"
+  has_many :books, through: :booksongs
 end
