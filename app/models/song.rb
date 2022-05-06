f818dd5226f89b90d1774c990d46908152f9f3d0
Brandon Lin <bwlin93@gmail.com>
2022-05-06 14:43:44 -0500

    adds logic to populate the YTExtension based on the url

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,4 +1,19 @@
 class Song < ApplicationRecord
   has_many :booksongs, foreign_key: "song_id", class_name: "BookSong"
   has_many :books, through: :booksongs
+
+  def YTExtension
+    start = url.index("v=") + 2
+    if url.include? "\u0026"
+      length = url.index("\u0026") - start
+      url[start, length]
+      # url[url.index("v=") + 3, url.index("\u0026") - url.index("v=") - 3]
+    else
+      length = url.length - start
+      url[start, length]
+
+      # url[url.index("v=") + 3, url.length url.index("v=") - 3]
+    end
+    # url.include? "\u0026"
+  end
 end
