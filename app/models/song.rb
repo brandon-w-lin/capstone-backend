6f9d0b5943ccf1f9d2b7be8f250f5df664f2778f
Brandon Lin <bwlin93@gmail.com>
2022-05-11 15:48:11 -0500

    adds validations for presence and uniqueness of the url. Cleans up test code.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,20 +1,18 @@
 class Song < ApplicationRecord
   has_many :booksongs, foreign_key: "song_id", class_name: "BookSong"
   has_many :books, through: :booksongs
+  validates :url, presence: true
+  validates :url, uniqueness: true
 
   def parseYTExtension(url)
     start = url.index("v=") + 2
     if url.include? "\u0026"
       length = url.index("\u0026") - start
       url[start, length]
-      # url[url.index("v=") + 3, url.index("\u0026") - url.index("v=") - 3]
     else
       length = url.length - start
       url[start, length]
-
-      # url[url.index("v=") + 3, url.length url.index("v=") - 3]
     end
-    # url.include? "\u0026"
   end
 
   def YTData()
@@ -23,18 +21,14 @@ class Song < ApplicationRecord
     return response.parse(:json)
   end
 
-  def test_method()
-    return "hello from method"
-  end
-
-  def queryYT(options)
-    query = ""
-    options.each do |k, v|
-      unless k == "controller" || k == "action"
-        query += "&#{k}=#{v}"
-      end
-    end
-    response = HTTP.get("https://youtube.googleapis.com/youtube/v3/videos?part=snippet&key=" + Rails.application.credentials.YT_data_api_key + query)
-    return response.parse(:json)
-  end
+  # def queryYT(options)
+  #   query = ""
+  #   options.each do |k, v|
+  #     unless k == "controller" || k == "action"
+  #       query += "&#{k}=#{v}"
+  #     end
+  #   end
+  #   response = HTTP.get("https://youtube.googleapis.com/youtube/v3/videos?part=snippet&key=" + Rails.application.credentials.YT_data_api_key + query)
+  #   return response.parse(:json)
+  # end
 end
