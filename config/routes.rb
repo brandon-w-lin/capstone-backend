3a37fd72018b02973db329a4a78a08c219d3759d
Brandon Lin <bwlin93@gmail.com>
2022-05-16 14:24:35 -0500

    adds route for getting related songs for a given book

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
+  get "/booksongs/book/:google_book_extension" => "books#get_songs"
+
   get "/songs" => "songs#index"
   get "/songs/id/:YT_extension" => "songs#show"
   post "/songs" => "songs#create"
