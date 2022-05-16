22d1a03cfdf042806567ebd8a699773910030e6f
Brandon Lin <bwlin93@gmail.com>
2022-05-16 14:22:18 -0500

    updates the booksong controller to use google_book_extension and YT_extension identifiers, fix create function to use those identifiers

--- app/controllers/book_songs_controller.rb
+++ app/controllers/book_songs_controller.rb
@@ -1,5 +1,6 @@
 class BookSongsController < ApplicationController
   before_action :set_book_song, only: %i[ show update destroy ]
+  include Rails.application.routes.url_helpers
 
   # GET /book_songs
   def index
@@ -15,7 +16,14 @@ class BookSongsController < ApplicationController
 
   # POST /book_songs
   def create
-    @book_song = BookSong.new(book_song_params)
+    @book_song = BookSong.new(
+      # Option 1: returns ID of book if already in database
+      # Option 2: returns ID of a new book that is created
+      book_id: Book.find_by(google_book_extension: params[:google_book_extension]).id,
+      song_id: Song.find_by(YT_extension: params[:YT_extension]).id,
+      google_book_extension: params[:google_book_extension],
+      YT_extension: params[:YT_extension],
+    )
 
     if @book_song.save
       render json: @book_song, status: :created, location: @book_song
@@ -52,6 +60,6 @@ class BookSongsController < ApplicationController
   # Only allow a list of trusted parameters through.
   def book_song_params
     # params.fetch(:book_song, {})
-    params.require(:book_song).permit(:book_id, :song_id)
+    params.require(:book_song).permit(:book_id, :song_id, :google_book_extension, :YT_extension)
   end
 end
