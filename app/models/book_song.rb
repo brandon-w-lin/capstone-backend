09595e5db4700d9b232146020a9a02a72d851cab
Brandon Lin <bwlin93@gmail.com>
2022-05-17 11:41:41 -0500

    removes unused function built during testing

--- app/models/book_song.rb
+++ app/models/book_song.rb
@@ -1,8 +1,4 @@
 class BookSong < ApplicationRecord
   belongs_to :book, class_name: "Book"
   belongs_to :song, class_name: "Song"
-
-  find_book_id(google_book_extension)
-    puts "test"
-  end
 end
