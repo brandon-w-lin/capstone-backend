f73fa8824fa3d1e764c8ba915004005022ac50b1
Brandon Lin <bwlin93@gmail.com>
2022-05-27 14:09:55 -0500

    adds another netlify link to CORS

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "localhost:8080", "while-you-read.netlify.app"
+    origins "localhost:8080", "while-you-read.netlify.app", "main--while-you-read.netlify.app"
 
     resource "*",
       headers: :any,
